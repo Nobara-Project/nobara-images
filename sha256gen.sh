fa082f2f3d96f0e99bc03de593e1a7116827d31c
GloriousEggroll <gloriouseggroll@gmail.com>
2024-07-17 10:37:51 -0600

    add sha256sum script

---
+++ sha256gen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd builds
+
+# Directory containing the files
+DIRECTORY="."
+
+# Iterate through each file in the directory
+for FILE in "$DIRECTORY"/*; do
+    # Check if it's a regular file
+    if [ -f "$FILE" ]; then
+        # Generate the SHA-256 checksum and save it to a file
+        sha256sum "$FILE" > "$FILE.sha256sum"
+        echo "Generated SHA-256 checksum for $FILE"
+    fi
+done
