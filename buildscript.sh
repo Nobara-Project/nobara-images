49c4793221cda52bf10123617f258f770c60f787
GloriousEggroll <gloriouseggroll@gmail.com>
2024-11-04 16:03:01 -0700

    put builds in builds folder in buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -3,49 +3,51 @@
 rm -Rf /var/lmc
 rm -Rf /var/run/anaconda.pid
 
+mkdir builds
+
 
 livemedia-creator --ks flat-nobara-live-official-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Official-$(date +%F).iso --releasever 40 --macboot
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks flat-nobara-live-gnome-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-GNOME-$(date +%F).iso --releasever 40 --macboot
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks flat-nobara-live-kde-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-KDE-$(date +%F).iso --releasever 40 --macboot
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks flat-nobara-live-steam-htpc-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Steam-HTPC-$(date +%F).iso --releasever 40 --macboot
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks flat-nobara-live-steam-handheld-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Steam-Handheld-$(date +%F).iso --releasever 40 --macboot
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks nv-flat-nobara-live-official-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Official-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks nv-flat-nobara-live-gnome-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-GNOME-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks nv-flat-nobara-live-kde-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-KDE-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
 livemedia-creator --ks nv-flat-nobara-live-steam-htpc-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Steam-HTPC-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
 
-mv /var/lmc/*.iso .
+mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
