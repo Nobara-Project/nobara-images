881812bb1797f31be102585865c32ac781d853d0
GloriousEggroll <gloriouseggroll@gmail.com>
2024-12-11 20:37:12 -0500

    convert buildscript to N41

--- buildscript.sh
+++ buildscript.sh
@@ -6,47 +6,47 @@ rm -Rf /var/run/anaconda.pid
 mkdir builds
 
 
-livemedia-creator --ks flat-nobara-live-official-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Official-$(date +%F).iso --releasever 40 --macboot
+livemedia-creator --ks flat-nobara-live-official-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-Official-$(date +%F).iso --releasever 41 --macboot
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks flat-nobara-live-gnome-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-GNOME-$(date +%F).iso --releasever 40 --macboot
+livemedia-creator --ks flat-nobara-live-gnome-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-GNOME-$(date +%F).iso --releasever 41 --macboot
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks flat-nobara-live-kde-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-KDE-$(date +%F).iso --releasever 40 --macboot
+livemedia-creator --ks flat-nobara-live-kde-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-KDE-$(date +%F).iso --releasever 41 --macboot
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks flat-nobara-live-steam-htpc-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Steam-HTPC-$(date +%F).iso --releasever 40 --macboot
+livemedia-creator --ks flat-nobara-live-steam-htpc-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-Steam-HTPC-$(date +%F).iso --releasever 41 --macboot
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks flat-nobara-live-steam-handheld-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Steam-Handheld-$(date +%F).iso --releasever 40 --macboot
+livemedia-creator --ks flat-nobara-live-steam-handheld-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-Steam-Handheld-$(date +%F).iso --releasever 41 --macboot
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks nv-flat-nobara-live-official-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Official-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks nv-flat-nobara-live-official-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-Official-Nvidia-$(date +%F).iso --releasever 41 --macboot --extra-boot-args "modules_load=nvidia"
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks nv-flat-nobara-live-gnome-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-GNOME-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks nv-flat-nobara-live-gnome-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-GNOME-Nvidia-$(date +%F).iso --releasever 41 --macboot --extra-boot-args "modules_load=nvidia"
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks nv-flat-nobara-live-kde-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-KDE-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks nv-flat-nobara-live-kde-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-KDE-Nvidia-$(date +%F).iso --releasever 41 --macboot --extra-boot-args "modules_load=nvidia"
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
 
-livemedia-creator --ks nv-flat-nobara-live-steam-htpc-40.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-40 --iso-only --iso-name Nobara-40-Steam-HTPC-Nvidia-$(date +%F).iso --releasever 40 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks nv-flat-nobara-live-steam-htpc-41.ks --no-virt --resultdir /var/lmc --project Nobara --make-iso --volid Nobara-41 --iso-only --iso-name Nobara-41-Steam-HTPC-Nvidia-$(date +%F).iso --releasever 41 --macboot --extra-boot-args "modules_load=nvidia"
 
 mv /var/lmc/*.iso builds/
 rm -Rf /var/lmc/
