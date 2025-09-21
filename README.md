4169722632c62b1e27f2febcaf7fc35bbe210559
GloriousEggroll <gloriouseggroll@gmail.com>
2025-09-21 04:17:37 -0600

    oops

--- README.md
+++ README.md
@@ -7,49 +7,49 @@ From within mock environment:
 
 Official:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-$(date +%F).iso --releasever 42 --macboot
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-$(date +%F).iso --releasever 42 --macboot
 ```
 
 Gnome:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-$(date +%F).iso --releasever 42 --macboot
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-$(date +%F).iso --releasever 42 --macboot
 ```
 
 KDE:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-$(date +%F).iso --releasever 42 --macboot
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-$(date +%F).iso --releasever 42 --macboot
 ```
 
 Steam HTPC:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-$(date +%F).iso --releasever 42 --macboot
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-$(date +%F).iso --releasever 42 --macboot
 ```
 
 Steam Handheld:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-steam-handheld-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-Handheld-$(date +%F).iso --releasever 42 --macboot
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/flat-nobara-live-steam-handheld-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-Handheld-$(date +%F).iso --releasever 42 --macboot
 ```
 
 
 
 Nvidia Official:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/nv-flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
 ```
 
 Nvidia Gnome:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/nv-flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
 ```
 
 Nvidia KDE:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/nv-flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
 ```
 
 Nvidia Steam HTPC:
 ```
-# livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-NV-$(date +%F).iso --releasever 42 --macboot
+# livemedia-creator --ks ISO-ready-flattened-kickstarts/42/nv-flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-NV-$(date +%F).iso --releasever 42 --macboot
 ```
 
 Finished!
--- build.sh
+++ build.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
+VERSION="42"
 mkdir complete
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-$(date +%F).iso --releasever 42 --macboot
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-$(date +%F).iso --releasever 42 --macboot
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-$(date +%F).iso --releasever 42 --macboot
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-$(date +%F).iso --releasever 42 --macboot
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-$(date +%F).iso --releasever 42 --macboot
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-$(date +%F).iso --releasever 42 --macboot
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-$(date +%F).iso --releasever 42 --macboot
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-$(date +%F).iso --releasever 42 --macboot
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/flat-nobara-live-steam-handheld-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-Handheld-$(date +%F).iso --releasever 42 --macboot
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/flat-nobara-live-steam-handheld-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-Handheld-$(date +%F).iso --releasever 42 --macboot
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/nv-flat-nobara-live-official-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Official-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/nv-flat-nobara-live-gnome-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-GNOME-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/nv-flat-nobara-live-kde-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-KDE-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks ISO-ready-flattened-kickstarts/nv-flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
+livemedia-creator --ks ISO-ready-flattened-kickstarts/${VERSION}/nv-flat-nobara-live-steam-htpc-42.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-42 --iso-only --iso-name Nobara-42-Steam-HTPC-NV-$(date +%F).iso --releasever 42 --macboot --extra-boot-args "modules_load=nvidia"
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
